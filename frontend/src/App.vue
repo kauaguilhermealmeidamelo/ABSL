bee0d0cc4fcfad5c4e3c2b01ce638d2b0ee6e04c
Kauã Guilherme Almeida Melo <kauagmelotrabalho@gmail.com>
2026-07-28 22:03:06 -0300

    estruturacao-do-site

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import api from './services/api'
+
+const resposta = ref('carregando...')
+
+onMounted(async () => {
+  try {
+    const { data } = await api.get('/ping')
+    resposta.value = data.message
+  } catch (error) {
+    resposta.value = 'erro ao conectar com a API'
+    console.error(error)
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>Teste de conexão com a API</h1>
+    <p>Resposta do backend: {{ resposta }}</p>
+  </div>
+</template>
