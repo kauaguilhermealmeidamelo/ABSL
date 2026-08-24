3deb5e94c5363eba88528b9ab39413441a8be3f9
Kauã Guilherme Almeida Melo <kauagmelotrabalho@gmail.com>
2026-08-24 16:09:33 -0300

    update text e icon

--- frontend/src/components/inicio/Sobre.vue
+++ frontend/src/components/inicio/Sobre.vue
@@ -38,6 +38,9 @@ const pillars = [
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,700;0,900;1,700;1,900&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,400;1,9..40,700&display=swap');
 
+p{
+  text-align: justify;
+}
 .about {
   margin-bottom: 40px;
   font-family: 'DM Sans', sans-serif;
